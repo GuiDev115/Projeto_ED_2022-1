6bb6777f5d3d2563eebc5c0661ce93ae1747541c
brunof5 <101609538+brunof5@users.noreply.github.com>
2022-06-30 18:02:25 -0300

    Atualização do programa de leitura

--- dev/leitura.cpp
+++ dev/leitura.cpp
@@ -7,8 +7,8 @@ using namespace std;
 struct CSV{
 	
 	int campo_1_id;
-	char campo_2_name[50];
-	char campo_3_job[50];
+	char campo_2_name[54];
+	char campo_3_job[54];
 	float campo_4_base_pay;
 	float campo_5_overtime_pay;
 	float campo_6_other_pay;
@@ -40,25 +40,25 @@ int main(){
 	
 	if(arq){
 		
-		CSV leitura[10];
+		CSV leitura[30];
 		
-		arq.seekg(0, arq.beg);
+		arq.seekg(140*357400, arq.beg);
 		
-		//arq.read((char*)(&leitura), n*sizeof(CSV));
+		//arq.read((char*)(&leitura), 10*sizeof(CSV));
 		arq.read((char*)(&leitura), sizeof(leitura));
 		
 		for(int i = 0; i < 10; i++){
 			
-			cout << leitura[i].campo_1_id << endl 
-			<< leitura[i].campo_2_name << endl 
-			<< leitura[i].campo_3_job << endl 
-			<< leitura[i].campo_4_base_pay << endl 
-			<< leitura[i].campo_5_overtime_pay << endl 
-			<< leitura[i].campo_6_other_pay << endl
-			<< leitura[i].campo_7_benefits << endl
-			<< leitura[i].campo_8_total_pay << endl
-			<< leitura[i].campo_9_total_pay_benefits << endl
-			<< leitura[i].campo_10_year << endl << endl;
+			cout << leitura[i].campo_1_id << '\n'
+			<< leitura[i].campo_2_name << '\n'
+			<< leitura[i].campo_3_job << '\n'
+			<< leitura[i].campo_4_base_pay << '\n'
+			<< leitura[i].campo_5_overtime_pay << '\n'
+			<< leitura[i].campo_6_other_pay << '\n'
+			<< leitura[i].campo_7_benefits << '\n'
+			<< leitura[i].campo_8_total_pay << '\n'
+			<< leitura[i].campo_9_total_pay_benefits << '\n'
+			<< leitura[i].campo_10_year << '\n' << endl;
 		}
 		
 		cout << sizeof(CSV) << " bytes" << endl;
@@ -67,7 +67,7 @@ int main(){
 		arq.seekg(0, arq.end);
 		int tam = arq.tellg();
 		
-		cout << tam << " bytes" << endl << endl;
+		cout << tam << " bytes" << endl;
 		
 		arq.close();
 	}
