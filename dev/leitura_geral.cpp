f9ce90f9cb6015c3e51d6c300eb527e35e2e373f
brunof5 <101609538+brunof5@users.noreply.github.com>
2022-07-06 16:19:07 -0300

    Pequenas alterações em leitura_geral.cpp

--- dev/leitura_geral.cpp
+++ dev/leitura_geral.cpp
@@ -40,6 +40,7 @@ Binario::Binario(){
 	campo_11_posicao = -1;
 }
 
+// Função para impressão dos dados
 void Binario::imprimir(Binario& umaLinha){
 	
 	cout << umaLinha.campo_1_id << '\n'
@@ -51,52 +52,58 @@ void Binario::imprimir(Binario& umaLinha){
 	<< umaLinha.campo_7_benefits << '\n'
 	<< umaLinha.campo_8_total_pay << '\n'
 	<< umaLinha.campo_9_total_pay_benefits << '\n'
-	<< umaLinha.campo_10_year << '\n' << endl;
+	<< umaLinha.campo_10_year << '\n'
+	<< umaLinha.campo_11_posicao << '\n' << endl;
+	
+	#warning retire o campo posicao antes de entregar!
 }
 
 int main(){
 	
-	ifstream arq("dados_convertidos.bin", ios::binary);
+	ifstream arq_binario("dados_convertidos.bin", ios::binary);
 	
-	if(arq){
+	if(arq_binario){
 		
-		arq.seekg(0, arq.end);
-		int tamanho_arquivo_binario = arq.tellg();
+		// É atribuído para tamanho_arquivo_binario quantos bytes há no arquivo binário
+		arq_binario.seekg(0, arq_binario.end);
+		int tamanho_arquivo_binario = arq_binario.tellg();
 		
+		// É atribuído para quantidade_linhas o total de linhas do arquivo binário
 		int quantidade_linhas = tamanho_arquivo_binario / sizeof(Binario);
 		
-		Binario dados_binario;
+		// Um ponteiro do tipo classe Binario é criado para ler cada linha do arquivo binário
 		Binario *leitura = new Binario[1];
 		
-		arq.seekg(0, arq.beg);
+		arq_binario.seekg(0, arq_binario.beg);
 		
+		// Variáveis para a execução das operações
 		char opcao;
 		bool controle = true, primeiro = false;
 		int aux_quant = quantidade_linhas;
 		
 		do{
-			
+			// É mostrado ao usuário 100 linhas por vez
 			int cont = 100;
 			
-			if(aux_quant >= 1 and aux_quant <= cont)
+			// Ou menos que 100 linhas, caso a quantidade de linhas restantes seja < cont
+			if(aux_quant >= 1 and aux_quant < cont)
 				cont = aux_quant;
 			
-			for(int i = 0; i < cont; i++){
+			// É lido uma linha de informações (um pacote da classe Binario) de cada vez
+			// Tal pacote é "inserido" no ponteiro leitura para vizualização
+			for(int i = 0; i < cont; i++, aux_quant--){
 				
-				arq.read((char*)(&leitura[0]), sizeof(Binario));
+				arq_binario.read((char*)(&leitura[0]), sizeof(Binario));
 				
-				dados_binario.imprimir(leitura[0]);
-				
-				aux_quant--;
+				leitura[0].imprimir(leitura[0]);
 			}
 			
-			cout << "Linhas restantes: " << aux_quant << endl;
-			
+			cout << "Linhas restantes: " << aux_quant << '\n';
 			cout << "Se desejar ver os resultados do ponto anterior até esse ponto, rode para cima" << endl;
 			
 			if(aux_quant == 0){
 				
-				cout << "Fim da aplicação, pressione enter para retornar" << endl;
+				cout << "Fim da aplicação, pressione enter para retornar. . . ";
 				
 				controle = false;
 				
@@ -105,8 +112,11 @@ int main(){
 			
 			else{
 				
-				cout << "Se deseja sair da aplicação, pressione q e enter, se não aperte enter para continuar" << endl;
+				cout << "Se deseja sair da aplicação, pressione q e enter, se não aperte enter para continuar. . . ";
 				
+				// O programa somente irá fechar se o 1º caractere (de uma sequência ou não) for 'q'
+				// Obs: essa é a função do bool primeiro
+				// Teoricamente, para qualquer outro caso o programa continuará 
 				while(opcao != '\n'){
 					
 					opcao = getchar();
@@ -117,22 +127,24 @@ int main(){
 					primeiro = true;
 				}
 				
-				opcao = ' ';
+				// É atribuído nulo à opcao para o programa não continuar rodando sozinho
+				opcao = 0;
 				primeiro = false;
 			}
 			
+			// No linux apenas um clear não basta, tal razão de ter dois comandos iguais
 			system("clear");
 			system("clear");
 			
 		}while(controle);
 		
-		arq.close();
+		arq_binario.close();
 		delete[] leitura;
 	}
 	
 	else{
 		
-		cout << "Não há arquivo convertido" << endl;
+		cout << "Não há arquivo convertido." << endl;
 	}
 	
 	return 0;
