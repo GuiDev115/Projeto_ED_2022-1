2085feae25b0737d58e081e30a9c9f6b5385b245
Guilherme Fabricio <guilherme.fabricio..115@gmail.com>
2022-07-07 11:12:55 -0300

    Criacao do HPP
    Deixando os outros codigos menores

---
+++ dev/classes.hpp
@@ -0,0 +1,34 @@
+class Binario{
+	
+	private:
+		int campo_1_id;
+		char campo_2_name[40];
+		char campo_3_job[54];
+		float campo_4_base_pay;
+		float campo_5_overtime_pay;
+		float campo_6_other_pay;
+		float campo_7_benefits;
+		float campo_8_total_pay;
+		float campo_9_total_pay_benefits;
+		int campo_10_year;
+		int campo_11_posicao;
+	
+	public:
+		Binario();
+		void imprimir(Binario& umaLinha);
+};
+
+Binario::Binario(){
+	
+	campo_1_id = -1;
+	memset(campo_2_name, 0, sizeof(campo_2_name));
+	memset(campo_3_job, 0, sizeof(campo_3_job));
+	campo_4_base_pay = 0;
+	campo_5_overtime_pay = 0;
+	campo_6_other_pay = 0;
+	campo_7_benefits = 0;
+	campo_8_total_pay = 0;
+	campo_9_total_pay_benefits = 0;
+	campo_10_year = 0;
+	campo_11_posicao = -1;
+}
--- dev/trocar_posicoes.cpp
+++ dev/trocar_posicoes.cpp
@@ -2,42 +2,9 @@
 #include <fstream>
 #include <cstring>
 
+#include "classes.hpp"
+
 using namespace std;
-
-class Binario{
-	
-	private:
-		int campo_1_id;
-		char campo_2_name[40];
-		char campo_3_job[54];
-		float campo_4_base_pay;
-		float campo_5_overtime_pay;
-		float campo_6_other_pay;
-		float campo_7_benefits;
-		float campo_8_total_pay;
-		float campo_9_total_pay_benefits;
-		int campo_10_year;
-		int campo_11_posicao;
-	
-	public:
-		Binario();
-		void trocar(fstream& umArquivo, int p1, int p2);
-};
-
-Binario::Binario(){
-	
-	campo_1_id = -1;
-	memset(campo_2_name, 0, sizeof(campo_2_name));
-	memset(campo_3_job, 0, sizeof(campo_3_job));
-	campo_4_base_pay = 0;
-	campo_5_overtime_pay = 0;
-	campo_6_other_pay = 0;
-	campo_7_benefits = 0;
-	campo_8_total_pay = 0;
-	campo_9_total_pay_benefits = 0;
-	campo_10_year = 0;
-	campo_11_posicao = -1;
-}
 
 // Função para trocar duas linhas (pacote/bloco classe Binario) de posição
 void Binario::trocar(fstream& umArquivo, int p1, int p2){
