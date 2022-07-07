76ab19597ba38009541ea9cadcc2f883823f755b
Guilherme Fabricio <guilherme.fabricio..115@gmail.com>
2022-07-07 12:44:17 -0300

    Criaçao da Main e divisao

---
+++ Finished/conversao.cpp
@@ -0,0 +1,373 @@
+#include <iostream>
+#include <cstring>
+#include <fstream>
+
+using namespace std;
+
+// Classe que representa cada linha do arquivo CSV, tamanho : 132 bytes
+class arquivo_CSV{
+	
+	private:
+		int campo_1_id;
+		// Feito um teste prévio, sabe-se que:
+		// o maior tamanho do campo do nome tem 39 + 1 (caractere fim da string: \0) = 40
+		// e o maior tamanho do campo do emprego tem 52 + 1 (caractere fim da string: \0) = 53, arredonda-se para 54
+		char campo_2_name[40];
+		char campo_3_job[54];
+		float campo_4_base_pay;
+		float campo_5_overtime_pay;
+		float campo_6_other_pay;
+		float campo_7_benefits;
+		float campo_8_total_pay;
+		float campo_9_total_pay_benefits;
+		int campo_10_year;
+		int campo_11_posicao;
+		
+	public:
+		arquivo_CSV();
+		void escrita_id(ifstream& umArquivo, arquivo_CSV& umaLinha);
+		void escrita_char(ifstream& umArquivo, arquivo_CSV& umaLinha, int seletor);
+		void escrita_pagamentos(ifstream& umArquivo, arquivo_CSV& umaLinha, int seletor);
+		void escrita_ano(ifstream& umArquivo, arquivo_CSV& umaLinha);
+		void escrita_posicao(arquivo_CSV& umaLinha, int& pos);
+};
+
+// Construtor
+arquivo_CSV::arquivo_CSV(){
+	
+	// A função memset foi vista pelo link: https://stackoverflow.com/questions/1559487/how-to-empty-a-char-array
+	// Nesse caso, basicamente o memset coloca um ponteiro na primeira posição do vetor de caracteres e vai percorrendo-o até seu término
+	// e imprime o valor, em cada caractere, que representa um número da tabela ascii (0 é o nulo)
+	
+	campo_1_id = -1;
+	memset(campo_2_name, 0, sizeof(campo_2_name));
+	memset(campo_3_job, 0, sizeof(campo_3_job));
+	campo_4_base_pay = 0;
+	campo_5_overtime_pay = 0;
+	campo_6_other_pay = 0;
+	campo_7_benefits = 0;
+	campo_8_total_pay = 0;
+	campo_9_total_pay_benefits = 0;
+	campo_10_year = 0;
+	campo_11_posicao = -1;
+}
+
+// Função para ler e escrever o id (campo 1) em binário
+void arquivo_CSV::escrita_id(ifstream& umArquivo, arquivo_CSV& umaLinha){
+	
+	char virgula;
+	int id;
+	
+	umArquivo >> id;
+	umArquivo >> virgula;
+	
+	umaLinha.campo_1_id = id;
+}
+
+// Função para ler e escrever o nome e trabalho (campos 2 e 3) em binário
+void arquivo_CSV::escrita_char(ifstream& umArquivo, arquivo_CSV& umaLinha, int seletor){
+	
+	// Pega a posição atual (em bytes) no arquivo CSV
+	umArquivo.seekg(0, umArquivo.cur);
+	int posicao_inicial = umArquivo.tellg();
+	
+	char c[1];
+	string texto;
+	getline(umArquivo, texto);
+	
+	// No arquivo CSV uma frase dentro de " " pode conter uma vírgula, logo a lógica a partir daqui cuida dessa operação
+	// Procura o primeiro caractere " dentro da string texto e retorna sua posição, se não for encontrado, retorna a última posição da string texto
+	size_t found_primeira_aspas = texto.find("\"");
+	
+	// A primeira " não foi encontrada
+	if(found_primeira_aspas == string::npos){
+		
+		// Retorna à posição inicial
+		umArquivo.seekg(posicao_inicial, umArquivo.beg);
+		
+		getline(umArquivo, texto, ',');
+		
+		switch(seletor){
+			
+			case 1:
+				for(unsigned int i = 0; i < texto.length(); i++)
+					umaLinha.campo_2_name[i] = texto[i];
+				break;
+				
+			case 2:
+				for(unsigned int i = 0; i < texto.length(); i++)
+					umaLinha.campo_3_job[i] = texto[i];
+				break;
+		}
+	}
+	
+	// A primeira " foi encontrada
+	else{
+		
+		// Procura a posição da vírgula
+		size_t found_virgula = texto.find(",");
+		
+		// Procura a segunda " dentro da string texto
+		size_t found_segunda_aspas = texto.find("\"", found_primeira_aspas + 1);
+		
+		// Se está condição for true, representa que há uma virgula antes da primeira ", ou seja, tal " pertence à outro campo (campo_3_job)
+		// Quando o campo_3_job estiver sendo analisado, ele não entra nessa condicional
+		if(found_virgula < found_primeira_aspas){
+			
+			umArquivo.seekg(posicao_inicial, umArquivo.beg);
+			
+			getline(umArquivo, texto, ',');
+			
+			for(unsigned int i = 0; i < texto.length(); i++)
+				umaLinha.campo_2_name[i] = texto[i];
+		}
+		
+		// A segunda " foi encontrada
+		else if(found_segunda_aspas != string::npos){
+			
+			umArquivo.seekg(posicao_inicial, umArquivo.beg);
+			unsigned int j = 0;
+			
+			// A variável j tem o papel de preencher o campo analisado, caractere a caractere, até a vírgula (último caractere " + 1)
+			while(j < found_segunda_aspas + 1){
+				
+				umArquivo.read(c, 1);
+				
+				switch(seletor){
+					
+					case 1:
+						umaLinha.campo_2_name[j] = c[0];
+						break;
+						
+					case 2:
+						umaLinha.campo_3_job[j] = c[0];
+						break;
+				}
+				
+				j++;
+			}
+			
+			// Remove a vírgula
+			umArquivo.read(c, 1);
+		}
+	}
+}
+
+// Função para ler e escrever os pagamentos (campos 4 a 9) em binário
+void arquivo_CSV::escrita_pagamentos(ifstream& umArquivo, arquivo_CSV& umaLinha, int seletor){
+	
+	char c[1], virgula;
+	string texto;
+	float pagamento;
+	
+	umArquivo.seekg(0, umArquivo.cur);
+	int posicao_inicial = umArquivo.tellg();
+	
+	umArquivo.read(c, 1);
+	
+	// No arquivo CSV, certos campos que deveriam ser para float na verdade possuem a informação: "Not Provided"
+	// Para não romper com a lógica dos pagamentos serem float, caso o primeiro caractere lido não seja um algarismo
+	// o devido pagamento terá valor igual a 0.
+	if(not(c[0] >= 48 and c[0] <= 57)){
+		
+		getline(umArquivo, texto, ',');
+		
+		pagamento = 0;
+		
+		switch(seletor){
+			
+			case 1:
+				umaLinha.campo_4_base_pay = pagamento;
+				break;
+				
+			case 2:
+				umaLinha.campo_5_overtime_pay = pagamento;
+				break;
+				
+			case 3:
+				umaLinha.campo_6_other_pay = pagamento;
+				break;
+				
+			case 4:
+				umaLinha.campo_7_benefits = pagamento;
+				break;
+				
+			case 5:
+				umaLinha.campo_8_total_pay = pagamento;
+				break;
+				
+			case 6:
+				umaLinha.campo_9_total_pay_benefits = pagamento;
+				break;
+		}
+	}
+	
+	else{
+		
+		umArquivo.seekg(posicao_inicial, umArquivo.beg);
+		
+		umArquivo >> pagamento;
+		
+		umArquivo >> virgula;
+		
+		switch(seletor){
+			
+			case 1:
+				umaLinha.campo_4_base_pay = pagamento;
+				break;
+				
+			case 2:
+				umaLinha.campo_5_overtime_pay = pagamento;
+				break;
+				
+			case 3:
+				umaLinha.campo_6_other_pay = pagamento;
+				break;
+				
+			case 4:
+				umaLinha.campo_7_benefits = pagamento;
+				break;
+				
+			case 5:
+				umaLinha.campo_8_total_pay = pagamento;
+				break;
+				
+			case 6:
+				umaLinha.campo_9_total_pay_benefits = pagamento;
+				break;
+		}
+	}
+}
+
+// Função para ler escrever o ano (campo 10) em binário
+void arquivo_CSV::escrita_ano(ifstream& umArquivo, arquivo_CSV& umaLinha){
+	
+	int ano;
+	
+	umArquivo >> ano;
+	
+	umaLinha.campo_10_year = ano;
+}
+
+// Função para escrever a posição de uma linha em binário
+// Tal valor será utilizado futuramente para outras operações
+void arquivo_CSV::escrita_posicao(arquivo_CSV& umaLinha, int& pos){
+	
+	umaLinha.campo_11_posicao = pos;
+	
+	pos++;
+}
+
+// Função para simular uma barra de progresso da conversão em CSV para binário. 
+// Fonte: https://stackoverflow.com/questions/14539867/how-to-display-a-progress-indicator-in-pure-c-c-cout-printf
+void progressbar(float &progresso){
+	
+	int tamanhoBarra = 70;
+	
+	cout << "[";
+	
+	// Desenha cada caractere da barra, baseado no valor de progresso
+	int pos = tamanhoBarra * progresso;
+	for(int i = 0; i < tamanhoBarra; i++){
+		
+		if(i < pos) 
+			cout << '=';
+		else if(i == pos) 
+			cout << '>';
+		else 
+			cout << " ";
+	}
+	
+	int porcento = int(progresso * 100.0);
+	
+	// \r move o cursor para o começo da linha, permitindo a reestruturação da barra
+	// cout.flush() libera um fluxo de saída (nesse caso, a barra de progresso)
+	if(porcento == 98){
+		
+		cout << "] " << 100 << " %\r";
+		cout.flush();
+	}
+	
+	else{
+		
+		cout << "] " << porcento << " %\r";
+		cout.flush();
+	}
+	
+	progresso += 0.01;
+}
+
+int main(){
+	
+	ifstream arq("san_francisco_payroll_dataset.csv");
+	
+	if(arq){
+		
+		// Lê a linha 1 do arquivo CSV que será descartada
+		string palavras_comeco;
+		getline(arq, palavras_comeco);
+		
+		// Cria ou sobrescreve o arquivo: dados_convertidos.bin
+		ofstream arq_binario("dados_convertidos.bin", ios::binary);
+		
+		// Um ponteiro é criado para ler cada linha do arquivo CSV
+		// O tamanho do ponteiro foi determinado previamente, olhando o número de total de Ids do arquivo CSV + 1
+		arquivo_CSV *linhas = new arquivo_CSV[357407];
+		int pos = 0;
+		
+		// Variáveis para para preencher a barra de progresso
+		float progresso = 0;
+		int divisor = 3574, porcento = 1;
+		
+		// Conversão dos dados
+		for(int i = 0; i < 357407; i++){
+			
+			int cont = 1;
+			
+			linhas[i].escrita_id(arq, linhas[i]);
+			
+			for(int j = 0; j < 2; j++){
+				
+				linhas[i].escrita_char(arq, linhas[i], cont);
+				
+				cont++;
+			}
+			
+			cont = 1;
+			
+			for(int j = 0; j < 6; j++){
+				
+				linhas[i].escrita_pagamentos(arq, linhas[i], cont);
+				
+				cont++;
+			}
+			
+			linhas[i].escrita_ano(arq, linhas[i]);
+			
+			linhas[i].escrita_posicao(linhas[i], pos);
+			
+			// Progressão da barra
+			if(porcento == (i / divisor)){
+				
+				progressbar(progresso);
+				porcento++;
+			}
+			
+			// Escrita de uma linha do arquivo CSV em binário
+			arq_binario.write((char*)(&linhas[i]), sizeof(linhas[i]));
+		}
+		
+		arq.close();
+		arq_binario.close();
+		delete[] linhas;
+		
+		cout << '\n' << "Conversão realizada, aperte enter para continuar. . . ";
+		
+		cin.get();
+	}
+	
+	else
+		cout << "Por favor, insira o arquivo escolhido ao grupo (san_francisco_payroll_dataset.csv) na pasta e reabra o programa." << endl;
+	
+	return 0;
+}
---
+++ Finished/main.cpp
@@ -0,0 +1,299 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <stdlib.h>
+
+#include "classes.hpp"
+
+using namespace std;
+
+int main(){
+
+    int n1;
+
+    cout << "Seja Bem vindo !!!" << endl;
+    cout << "Por Favor faca sua escolha para progredir no programa" << endl << endl;
+
+    cout << "1 - Inserir || 2 - Leitura Geral || 3 - Leitura em determinado espaco || 4 - Trocar Posicao" >> endl;
+
+    cin >> n1;
+
+    switch(n1){
+        case 1:
+            cout << "Fazer";
+        break;
+
+        case 2:{
+
+            ifstream arq_binario02("dados_convertidos.bin", ios::binary);
+	
+	        if(arq_binario02){
+		
+            // É atribuído para tamanho_arquivo_binario quantos bytes há no arquivo binário
+            arq_binario02.seekg(0, arq_binario02.end);
+            int tamanho_arquivo_binario = arq_binario02.tellg();
+            
+            // É atribuído para quantidade_linhas o total de linhas do arquivo binário
+            int quantidade_linhas = tamanho_arquivo_binario / sizeof(Binario);
+            
+            // Um ponteiro do tipo classe Binario é criado para ler cada linha do arquivo binário
+            Binario *leitura = new Binario[1];
+            
+            arq_binario02.seekg(0, arq_binario02.beg);
+            
+            // Variáveis para a execução das operações
+            char opcao;
+            bool controle = true, primeiro = false;
+            int aux_quant = quantidade_linhas;
+            
+            do{
+                // É mostrado ao usuário 100 linhas por vez
+                int cont = 100;
+                
+                // Ou menos que 100 linhas, caso a quantidade de linhas restantes seja < cont
+                if(aux_quant >= 1 and aux_quant < cont)
+                    cont = aux_quant;
+                
+                // É lido uma linha de informações (um pacote da classe Binario) de cada vez
+                // Tal pacote é "inserido" no ponteiro leitura para vizualização
+                for(int i = 0; i < cont; i++, aux_quant--){
+                    
+                    arq_binario02.read((char*)(&leitura[0]), sizeof(Binario));
+                    
+                    leitura[0].imprimir(leitura[0]);
+                }
+                
+                cout << "Linhas restantes: " << aux_quant << '\n';
+                cout << "Se desejar ver os resultados do ponto anterior até esse ponto, rode para cima" << endl;
+                
+                if(aux_quant == 0){
+                    
+                    cout << "Fim da aplicação, pressione enter para retornar. . . ";
+                    
+                    controle = false;
+                    
+                    cin.get();
+                }
+                
+                else{
+                    
+                    cout << "Se deseja sair da aplicação, pressione q e enter, se não aperte enter para continuar. . . ";
+                    
+                    // O programa somente irá fechar se o 1º caractere (de uma sequência ou não) for 'q'
+                    // Obs: essa é a função do bool primeiro
+                    // Teoricamente, para qualquer outro caso o programa continuará 
+                    while(opcao != '\n'){
+                        
+                        opcao = getchar();
+                        
+                        if((!primeiro) and (opcao == 'q'))
+                            controle = false;
+                            
+                        primeiro = true;
+                    }
+                    
+                    // É atribuído nulo à opcao para o programa não continuar rodando sozinho
+                    opcao = 0;
+                    primeiro = false;
+                }
+                
+                // No linux apenas um clear não basta, tal razão de ter dois comandos iguais
+                system("clear");
+                system("clear");
+                
+            }while(controle);
+            
+            arq_binario02.close();
+            delete[] leitura;
+        }
+        
+        else
+            cout << "Não há arquivo convertido." << endl;
+        break;
+    }
+
+        case 3:{
+            // erro será o retorno do programa, que é tratado no programa principal
+	        // Se erro = 1, representa que ocorreu erro (o usuário digitou algo incorreto)
+	        int erro03 = 0;
+	
+	        ifstream arq_binario03("dados_convertidos.bin", ios::binary);
+	
+	        if(arq_binario03){
+		
+		        // É atribuído para tamanho_arquivo_binario quantos bytes há no arquivo binário
+		        arq_binario03.seekg(0, arq_binario03.end);
+		        int tamanho_arquivo_binario = arq_binario03.tellg();
+		
+		        // Um ponteiro do tipo classe Binario é criado para ler as linhas desejadas do arquivo binário
+                Binario *leitura = new Binario[1];
+                
+                arq_binario03.seekg(0, arq_binario03.beg);
+                
+                // Variáveis para a execução das operações
+                int inicial_pos = -1;
+                int final_pos = -1;
+                long aux;
+                
+                cout << "Escreva a posição inicial: " << endl;
+                cin >> inicial_pos;
+                
+                // Tamanho da inicial_pos em relação ao início do arquivo binário,
+                // ou seja, representa o byte que a posição de inicial_pos começa
+                aux = sizeof(Binario) * inicial_pos;
+		
+                if((inicial_pos < 0) or (aux > tamanho_arquivo_binario) or (cin.fail()))
+                    erro03 = 1;
+		
+                else{
+                    
+                    cout << "Escreva a posição final: " << endl;
+                    
+                    cin >> final_pos;
+                    
+                    // Tamanho da final_pos em relação ao início do arquivo binário,
+                    // ou seja, representa o byte que a posição de final_pos começa
+                    aux = sizeof(Binario) * final_pos;
+                    
+                    if((final_pos < inicial_pos) or (final_pos < 0) or (aux > tamanho_arquivo_binario) or (cin.fail()))
+                        erro03 = 1;
+                    
+                    else{
+                        
+                        // Intervalo de leitura
+                        int intervalo = final_pos - inicial_pos + 1;
+                        
+                        arq_binario03.seekg(inicial_pos * sizeof(Binario), arq_binario03.beg);
+                        
+                        cout << '\n' << "Resultado: " << '\n' << '\n';
+                        
+                        bool primeiro = false;
+                        
+                        do{
+                            // É mostrado ao usuário 100 linhas por vez
+                            int cont = 100;
+                            
+                            // Ou menos que 100 linhas, caso o intervalo restante seja < cont
+                            if(intervalo >= 1 and intervalo < cont)
+                                cont = intervalo;
+                                
+                            // É lido uma linha de informações (um pacote da classe Binario) de cada vez
+                            // Tal pacote é "inserido" no ponteiro leitura para vizualização
+                            for(int i = 0; i < cont; i++, intervalo--){
+                                
+                                arq_binario03.read((char*)(&leitura[0]), sizeof(Binario));
+                                
+                                leitura[0].imprimir(leitura[0]);
+                            }
+                            
+                            cout << "Linhas restantes: " << intervalo << '\n';
+                            cout << "Se desejar ver os resultados do ponto anterior até esse ponto, rode para cima" << endl;
+                            
+                            if(intervalo != 0)
+                                cout << "Pressione enter para continuar. . . ";
+                                
+                            else
+                                cout << "Pressione enter para sair. . . ";
+                            
+                            // Ignora o primeiro enter
+                            if(!primeiro){
+                                
+                                cin.ignore();
+                                
+                                primeiro = true;
+                            }
+                            
+                            // O programa apenas irá continuar/parar após o usuário pressionar enter.
+                            // Tal laço é feito para impedir que a cada caractere que o usuário digite, 
+                            // o programa rode uma vez + quantidade de caracteres inseridos
+                            bool controle = true;
+                            while(controle){
+                                
+                                if(cin.get() == '\n')
+                                    controle = false;
+                            }
+                            
+                            // No linux apenas um clear não basta, tal razão de ter dois comandos iguais
+                            system("clear");
+                            system("clear");
+                            
+                        }while(intervalo != 0);
+                    }
+                }
+                arq_binario03.close();
+                delete[] leitura;
+            }
+            else
+                cout << "Não há arquivo convertido" << endl;
+            return erro03;
+        break;
+        }
+
+        case 4:{
+            // O arquivo binário é aberto tanto para entrada quanto saída de dados
+	        fstream arq_binario04("dados_convertidos.bin", ios::binary | ios::in | ios::out);
+	
+            // erro será o retorno do programa, que é tratado no programa principal
+            // Se erro = 1, representa que ocorreu erro (o usuário digitou algo incorreto)
+            // Se erro = 2, os valores das posições são iguais
+            int erro04 = 0;
+            
+            if(arq_binario04){
+                
+                // É atribuído para tamanho_arquivo_binario quantos bytes há no arquivo binário
+                arq_binario04.seekg(0, arq_binario04.end);
+                int tamanho_arquivo_binario = arq_binario04.tellg();
+                
+                // É criado uma variável do tipo classe Binario para acessar a função de trocar
+                Binario dados_binario;
+                
+                // Variáveis para a execução das operações
+                int pos_primeira = -1;
+                int pos_segunda = -1;
+                long aux;
+                
+                cout << "Escreva a primeira posição da troca: " << endl;
+                cin >> pos_primeira;
+                
+                // Tamanho da pos_primeira em relação ao início do arquivo binário,
+                // ou seja, representa o byte que a posição de pos_primeira começa
+                aux = pos_primeira * sizeof(Binario);
+                
+                if((pos_primeira < 0) or (aux > tamanho_arquivo_binario) or (cin.fail()))
+                    erro04 = 1;
+                    
+                else{
+                    
+                    cout << "Escreva a segunda posição da troca: " << endl;
+                    cin >> pos_segunda;
+                    
+                    // Tamanho da pos_segunda em relação ao início do arquivo binário,
+                    // ou seja, representa o byte que a posição de pos_segunda começa
+                    aux = sizeof(Binario) * pos_segunda;
+                    
+                    if((pos_segunda < 0) or (aux > tamanho_arquivo_binario) or (cin.fail()))
+                        erro04 = 1;
+                        
+                    else{
+                        
+                        if(pos_primeira == pos_segunda)
+                            erro04 = 2;
+                            
+                        else
+                            dados_binario.trocar(arq_binario04, pos_primeira, pos_segunda);
+                    }
+                }
+                
+                arq_binario04.close();
+            }
+            
+            else{
+                
+                cout << "Não há arquivo convertido" << endl;
+            }
+            
+            return erro04;
+        break;
+        }
+    }
+}
