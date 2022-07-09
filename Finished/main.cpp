144ad133b8a9621f1b3a00fa69d6b0a651c1e9e2
brunof5 <101609538+brunof5@users.noreply.github.com>
2022-07-09 14:08:59 -0300

    Update main.cpp

--- Finished/main.cpp
+++ Finished/main.cpp
@@ -17,11 +17,11 @@ int main(){
 	Binario *bin = new Binario[1];
 	
 	cout << "||||                   Seja Bem vindo !!!                  ||||" << '\n';
-    cout << "|||| Por Favor faca sua escolha para progredir no programa ||||" << '\n' << '\n';
+    cout << "|||| Por Favor faça sua escolha para progredir no programa ||||" << '\n' << '\n';
 	
 	do{
 		
-		cout << "|| 1 - Inserir || 2 - Leitura Geral || 3 - Leitura em determinado espaco || 4 - Trocar Posicao || 5 - Alterar Dados || 0 - Sair ||" << '\n';
+		cout << "|| 1 - Inserir || 2 - Leitura Geral || 3 - Leitura em determinado espaço || 4 - Trocar Posição || 5 - Alterar Dados || 0 - Sair ||" << '\n';
 		getline(cin, dado);
 		
 		if((dado.size() > 1) or (!((dado[0] >= 48) and (dado[0] <= 53))) or (cin.fail()))
@@ -32,16 +32,11 @@ int main(){
 			case 1:{
 				bin -> inserir();
 				
-				
 				break;
 			}
 			
 			case 2:{
 				bin -> leitura_geral();
-				
-				cout << "Fim da aplicação, pressione enter para retornar. . . ";   
-				cin.get();
-				system("clear");
 				
 				break;
 			}
@@ -74,9 +69,12 @@ int main(){
 				else if(result == 2)
 					cout << "As posições são iguais, não há porque trocar" << endl;
 					
-				cout << "Pressione enter para continuar. . .";
-				cin.ignore();
-				cin.get();
+				if((result >= 0 and result <= 2)){
+					
+					cout << "Pressione enter para continuar. . .";
+					cin.ignore();
+					cin.get();
+				}
 				
 				break;
 			}
@@ -99,14 +97,17 @@ int main(){
 				else if(result == 2)
 					cout << "O que foi digitado não corresponde com nenhum item da lista" << endl;
 				
-				cout << "Pressione enter para continuar. . .";
-				cin.get();
+				if((result >= 0 and result <= 2)){
+					
+					cout << "Pressione enter para continuar. . .";
+					cin.get();
+				}
 				
 				break;
 			}
 			
 			case 0:{
-				cout << endl << "Ate mais !!!! Fechando programa...";
+				cout << endl << "Até mais !!!! Fechando programa...";
 				controle = false;
 				break;
 			}
