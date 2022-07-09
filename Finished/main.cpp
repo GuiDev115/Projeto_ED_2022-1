bc7828f0e74f30a808e1abf59ed746e2cdfdce4d
brunof5 <101609538+brunof5@users.noreply.github.com>
2022-07-09 05:00:54 -0300

    Arrumando indentação e alguns casos de teste na main

--- Finished/main.cpp
+++ Finished/main.cpp
@@ -4,65 +4,99 @@
 #include <stdlib.h>
 
 #include "classes.hpp"
-//Chamada do arquivo auxiliar .hpp na onde escontra-se maior parte do algoritmo do projeto. Desde inserir até a alteração.
-//Construtores e classe estão colocados tambem no .hpp
+// Chamada do arquivo auxiliar .hpp na onde escontra-se maior parte do algoritmo do projeto. Desde inserir até a alteração.
+// Construtores e classe estão colocados tambem no .hpp
 
 using namespace std;
 
 int main(){
-
-    int dado;
-
-    Binario *bin = new Binario[1];
-
-    cout << "Seja Bem vindo !!!" << endl;
-    cout << "Por Favor faca sua escolha para progredir no programa" << endl << endl;
-
-    cout << "1 - Inserir || 2 - Leitura Geral || 3 - Leitura em determinado espaco || 4 - Trocar Posicao || 5 - Alterar Dados || 0 - Sair" << endl;
-
-    cin >> dado;
-
-    switch(dado){
-
-        case 1:
-            cout << "Fazer";
-        break;
-
-        case 2:{
+	
+	string dado = " ";
+	bool controle = true;
+	
+	Binario *bin = new Binario[1];
+	
+	cout << "Seja Bem vindo !!!" << endl;
+	cout << "Por Favor faca sua escolha para progredir no programa" << endl << endl;
+	
+	do{
+		
+		cout << "1 - Inserir || 2 - Leitura Geral || 3 - Leitura em determinado espaco || 4 - Trocar Posicao || 5 - Alterar Dados || 0 - Sair" << endl;
+		getline(cin, dado);
+		
+		if((dado.size() > 1) or (!((dado[0] >= 48) and (dado[0] <= 53))) or (cin.fail()))
+			dado = " ";
 			
-			cin.ignore();
-            bin->leitura_geral();
-            break;
-        }
-
-        case 3:{
-
-            int erro03 = 0;
-            bin -> leitura_por_espaco(erro03);
-
-        break;
-        }
-
-        case 4:{
-
-            int erro04 = 0;
-            bin -> troca_pos(erro04);
-
-        break;
-        }
-
-        case 5:{
-            int erro05 = 0;
-            bin -> alterar(erro05);
-
-        break;
-        }
-
-        default:{
-            cout << endl <<"Ate mais !!!! Fechando programa...";
-
-            break;
-        }
-    }
-    return 0;
+		else
+			
+		
+		switch(int(dado[0] - 48)){
+			
+			case 1:{
+				cout << "Fazer";
+				cin.get();
+				break;
+			}
+			
+			case 2:{
+				bin -> leitura_geral();
+				
+				break;
+			}
+			
+			case 3:{
+				int erro03 = 0, result = 0;
+				result = bin -> leitura_por_espaco(erro03);
+				
+				if(result == 1)
+					cout << "Posição inválida" << endl;
+				
+				break;
+			}
+			
+			case 4:{
+				int erro04 = 0, result = 0;
+				
+				result = bin -> troca_pos(erro04);
+				
+				if(result == 1)
+					cout << "Posição inválida" << endl;
+					
+				else if(result == 2)
+					cout << "As posições são iguais, não há porque trocar" << endl;
+					
+				cin.ignore();
+				cin.get();
+				
+				break;
+			}
+			
+			case 5:{
+				int erro05 = 0, result = 0;
+				
+				result = bin -> alterar(erro05);
+				
+				if(result == 1)
+					cout << "Posição inválida" << endl;
+					
+				else if(result == 2)
+					cout << "O que foi digitado não corresponde com nenhum item da lista" << endl;
+				
+				cin.get();
+				
+				break;
+			}
+			
+			case 0:{
+				cout << endl << "Ate mais !!!! Fechando programa...";
+				controle = false;
+				break;
+			}
+		}
+		
+		if(dado[0] != '0')
+			system("clear");
+	}while(controle);
+	
+	return 0;
 }
